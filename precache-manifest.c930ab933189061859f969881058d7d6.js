338aeff8e8d2ea9f1551db6bf64e2a26f8110e82
sorc97 <nnd64809200@gmail.com>
2020-02-28 12:10:28 +0300

    Updates

---
+++ precache-manifest.c930ab933189061859f969881058d7d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdecb0140cc700d11238c6aaedc96046",
+    "url": "/AmazingHiringTest/index.html"
+  },
+  {
+    "revision": "cb183fa8fabf6bacfbfe",
+    "url": "/AmazingHiringTest/static/css/main.3993ac52.chunk.css"
+  },
+  {
+    "revision": "2d16cc13fd36dc640c96",
+    "url": "/AmazingHiringTest/static/js/2.618130a3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/AmazingHiringTest/static/js/2.618130a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb183fa8fabf6bacfbfe",
+    "url": "/AmazingHiringTest/static/js/main.31a62e85.chunk.js"
+  },
+  {
+    "revision": "f22304e2f61d0b876fa3",
+    "url": "/AmazingHiringTest/static/js/runtime-main.64cad996.js"
+  }
+]);
